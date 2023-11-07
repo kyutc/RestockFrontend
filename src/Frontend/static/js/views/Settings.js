d272c941bfabf2a43c9eed38763e8e1c8cc65903
ivanxcc <96937482+ivanxcc@users.noreply.github.com>
2023-11-07 15:03:36 -0500

    Added group and account management sections

--- src/Frontend/static/js/views/Settings.js
+++ src/Frontend/static/js/views/Settings.js
@@ -10,6 +10,26 @@ export default class extends AbstractView {
         return `
             <h1>Settings</h1>
             <p>Manage your privacy and configuration.</p>
+
+            <section id="group-management">
+                <h3>Group Management</hh3>
+
+                <!-- Form to create a new group -->
+                <form id="create-group-form">
+                    <input type="text" id="group-name" placeholder="Group Name" required />
+                    <button type="submit" id="create-group">Create Group</button>
+                </form>
+                    
+                <!-- List of Existing groups -->
+                <div id="groups-list">
+                    <!-- TODO: Groups will be populated here -->
+                </div>
+            </section>
+
+            <section id="account-managemnt">
+                <h3>Account Management</h3>
+                <button id="delete-account">Delete Account</button>
+            </section>
         `;
     }
 }
