9594980f1929ac1ff5e0fa1891ce0c81bd795f09
ABViney <vineyadrian@gmail.com>
2023-12-07 23:15:06 -0500

    change css class names

--- src/views/components/inventory/pantry_item.js
+++ src/views/components/inventory/pantry_item.js
@@ -1,3 +1,5 @@
+import itemOptionsMenu from "./item_options_menu.js";
+
 /**
  * Each <ion-item> found in the <ion-list id="pantry-content"> element (of inventory_content_frame.js) is generated
  * using this component.
@@ -13,6 +15,6 @@ export default function pantryItemComponent(item) {
         <ion-label>${item.name}</ion-label>
         <ion-chip class="ion-float-right" id="p-${item.id}">${item.pantry_quantity}</ion-chip>
         <ion-button class="subtract-pantry"><ion-icon name="remove-outline"></ion-icon></ion-button>
-        <ion-button class="options"><ion-icon name="ellipsis-vertical-outline"></ion-icon></ion-button>
+        <ion-button class="pantry-options"><ion-icon name="ellipsis-vertical-outline"></ion-icon></ion-button>
     </ion-item>`;
 }
--- src/views/components/inventory/shopping_list_item.js
+++ src/views/components/inventory/shopping_list_item.js
@@ -7,11 +7,11 @@
 export default function shoppingListItemComponent(item) {
     return `
         <ion-item id="${item.id}" ${item.shopping_list_quantity == 0 ? 'class="hidden"' : ''}>
-            <ion-button class="add-shopping_list"><ion-icon name="add-outline"></ion-icon></ion-button>
+            <ion-button class="add-shopping-list"><ion-icon name="add-outline"></ion-icon></ion-button>
             <ion-label>${item.name}</ion-label>
             <ion-chip class="ion-float-right" id="sl-${item.id}">${item.shopping_list_quantity}</ion-chip>
-            <ion-button class="subtract-shopping_list"><ion-icon name="remove-outline"></ion-icon></ion-button>
-            <ion-button class="options"><ion-icon name="ellipsis-vertical-outline"></ion-icon></ion-button>
+            <ion-button class="subtract-shopping-list"><ion-icon name="remove-outline"></ion-icon></ion-button>
+            <ion-button class="shopping-list-options"><ion-icon name="ellipsis-vertical-outline"></ion-icon></ion-button>
         </ion-item>
     `;
 }
