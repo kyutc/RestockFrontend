479b86bfd91435f86e4379bbd8f0736efdff888e
ABViney <vineyadrian@gmail.com>
2023-12-10 16:14:31 -0500

    add timestamp to group history

--- src/views/components/inventory/history_item.js
+++ src/views/components/inventory/history_item.js
@@ -5,8 +5,9 @@ import ActionLog from "../../../models/action_log.js";
  */
 export default function historyItem(action_log) {
     return `
-        <ion-item id="h-${action_log.id}">
+        <ion-item button="true" id="h-${action_log.id}">
             <ion-label color="medium">${action_log.log_message}</ion-label>
+            <ion-chip color="darm">${new Date(action_log.timestamp * 1000).toDateString()}</ion-chip>
         </ion-item>
     `;
 }
